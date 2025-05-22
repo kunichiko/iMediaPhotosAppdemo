d9743d742700a4fe7c61b461f50d180cefccbbe5
Kunihiko Ohnaka <ohnaka@ripplex.com>
2025-05-22 09:34:09 +0900

    Initial Commit

---
+++ iMediaPhotosAppDemoTests/iMediaPhotosAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  iMediaPhotosAppDemoTests.swift
+//  iMediaPhotosAppDemoTests
+//
+//  Created by Kunihiko Ohnaka on 2025/05/22.
+//
+
+import Testing
+@testable import iMediaPhotosAppDemo
+
+struct iMediaPhotosAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
